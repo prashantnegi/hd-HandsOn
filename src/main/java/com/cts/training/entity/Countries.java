804b2b9594b8ad2f6bfc6b0fa6c40db3cda7971a
prashantnegi <32644390+prashantnegi@users.noreply.github.com>
2020-12-21 22:30:13 +0530

    Add files via upload

---
+++ src/main/java/com/cts/training/entity/Countries.java
@@ -0,0 +1,45 @@
+package com.cts.training.entity;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Countries {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "country_id")
+	private Integer id;
+	
+	@Column(name = "country_name")
+	private String countryName;
+	
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "region_id", nullable = false)
+	private Regions region;
+	
+	@OneToMany(mappedBy = "country", cascade = CascadeType.ALL)
+	private List<Locations> location = new ArrayList<>();
+}
---
+++ src/main/java/com/cts/training/entity/Locations.java
@@ -0,0 +1,56 @@
+package com.cts.training.entity;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Locations {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "location_id")
+	private Integer id;
+	
+	@Column(name = "street_address")
+	private String streetAddress;
+	
+	@Column(name = "postal_code")
+	private Integer postalCode;
+	
+	@Column(nullable = false)
+	private String city;
+	
+	@Column(name = "state_province")
+	private String stateProvince;
+	
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "country_id", nullable = false)
+	private Countries country;
+	
+	@OneToMany(mappedBy = "location", cascade = CascadeType.ALL)
+	private List<Departments> departments = new ArrayList<>();
+	
+	
+}
